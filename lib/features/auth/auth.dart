c3f2923214f7e9eccb9dbd67f7db0084e4b3be43
ttoghere <tuncsoftwarehere@gmail.com>
2023-04-18 18:30:29 +0300

    What i did ?? && Next on:
    Did?
    1)Added assets
    2)Deal with Xcode issues
    3)Landing screen is done
    4)Storing user files to firebase is done
    5)First version of widget/global constants are done
    Next on:
    Displaying contacts in user's phone

---
+++ lib/features/auth/auth.dart
@@ -0,0 +1,5 @@
+export 'controller/auth_controller.dart';
+export 'repository/auth_repository.dart';
+export 'screens/login_screen.dart';
+export 'screens/otp_screen.dart';
+export 'screens/user_information_screen.dart';
---
+++ lib/router.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:tkchatv2/common/widgets/error.dart';
+import 'package:tkchatv2/features/auth/auth.dart';
+
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case LoginScreen.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const LoginScreen(),
+      );
+    case OTPScreen.routeName:
+      final verificationId = settings.arguments as String;
+      return MaterialPageRoute(
+        builder: (context) => OTPScreen(
+          verificationId: verificationId,
+        ),
+      );
+    case UserInformationScreen.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const UserInformationScreen(),
+      );
+    default:
+      return MaterialPageRoute(
+        builder: (context) => const Scaffold(
+          body: ErrorScreen(error: 'This page doesn\'t exist'),
+        ),
+      );
+  }
+}
